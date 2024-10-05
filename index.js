0e9755d8578cb70884cbb2572b02dfdc4dc05cff
Nastya <anastasia.zubar26@gmail.com>
2024-10-05 21:35:07 +0200

    format

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let time = +prompt('What time is now?')
 const log = console.log
 
 
-if (time < 0){
+if (time < 0) {
     log('There is no such time')
-} else if (time > 24){
+} else if (time > 24) {
     log('There is no such time')
-} else if (time < 6){
+} else if (time < 6) {
     log('Night')
-} else if (time < 12){
+} else if (time < 12) {
     log('Morning')
-} else if (time < 18){
+} else if (time < 18) {
     log('Afrernoon')
-} else if (tine < 24){
+} else if (tine < 24) {
     log('Evening')
 }
